3c712d389009ea67da767c23b9b6b12396f22c1f
CHANI <115384067+ChaniDev@users.noreply.github.com>
2022-11-11 03:56:44 +0530

    Mess CleanUp

--- Tic Tac Toe/Assets/Scripts/InputID.cs
+++ Tic Tac Toe/Assets/Scripts/InputID.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class InputID : MonoBehaviour
 {
-    
+    int locationTrigger;
+
+    void OnMouseDown()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            locationTrigger = System.Convert.ToInt32(this.gameObject.name);
+        }
+    }
 }
--- Tic Tac Toe/Assets/Scripts/InputManager.cs
+++ Tic Tac Toe/Assets/Scripts/InputManager.cs
@@ -4,23 +4,5 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    InputID insInputID;
-
-//-----
-
-
-    void Start()
-    {
-        insInputID = FindObjectOfType<InputID>();
-    }
-
-    public int RequestP1Input()
-    {
-        return(10);
-    }
-
-    public int RequestP2Input()
-    {
-        return(10);
-    }
+    
 }
