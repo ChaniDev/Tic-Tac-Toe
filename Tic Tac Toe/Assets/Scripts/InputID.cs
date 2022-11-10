ec6e9ae01e8452d819f30937748f6a3c12ac08ff
CHANI <115384067+ChaniDev@users.noreply.github.com>
2022-11-11 00:40:06 +0530

    -WIP-

--- Tic Tac Toe/Assets/Scripts/InputID.cs
+++ Tic Tac Toe/Assets/Scripts/InputID.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InputID : MonoBehaviour
 {
     InputManager insInputManager;
+    
     string TriggerID = "Default";
 
     void Start()
--- Tic Tac Toe/Assets/Scripts/InputManager.cs
+++ Tic Tac Toe/Assets/Scripts/InputManager.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    
+    List<int> availableSlots;
+
+    void Start()
+    {
+            
+    }
 }
