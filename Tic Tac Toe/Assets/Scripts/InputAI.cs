79c76acca8fb6f408761f3fa1b535998af7d3885
CHANI <115384067+ChaniDev@users.noreply.github.com>
2022-11-20 07:41:08 +0530

    Clean Up

--- Tic Tac Toe/Assets/Scripts/InputAI.cs
+++ Tic Tac Toe/Assets/Scripts/InputAI.cs
@@ -4,18 +4,5 @@ using UnityEngine;
 
 public class InputAI : MonoBehaviour
 {
-    int selectedSlot = 0;
     
-
-    public int locationTrigger(List<int> validSlots)
-    {
-        if(validSlots.Count <= 0)
-        {
-            return(0);
-        }
-
-           
-
-        return(selectedSlot);
-    }
 }
--- Tic Tac Toe/Assets/Scripts/InputPlayer.cs
+++ Tic Tac Toe/Assets/Scripts/InputPlayer.cs
@@ -4,23 +4,5 @@ using UnityEngine;
 
 public class InputPlayer : MonoBehaviour
 {
-    InputManager insInputManager;
-
-//----
-    int locationTrigger;
-
-    void Start()
-    {
-        insInputManager = FindObjectOfType<InputManager>();
-    }
-
-    void OnMouseDown()
-    {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            locationTrigger = System.Convert.ToInt32(this.gameObject.name);
-
-            insInputManager.PlayerInput(locationTrigger);
-        }
-    }
+    
 }
