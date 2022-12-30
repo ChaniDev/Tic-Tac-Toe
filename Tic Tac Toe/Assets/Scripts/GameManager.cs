f506bf1c87334cf23e9079e90b9762c9a1c266ac
CHANI <115384067+ChaniDev@users.noreply.github.com>
2022-12-30 06:30:10 +0530

    A better tracking solution

--- Tic Tac Toe/Assets/Scripts/GameManager.cs
+++ Tic Tac Toe/Assets/Scripts/GameManager.cs
@@ -5,33 +5,57 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+
     InputManager insInputManager;
+    InputAI insInputAI;
+
+    [SerializeField] private bool AI = true;
 
     [SerializeField] private GameObject Cross;
     [SerializeField] private GameObject Circle;
+    [SerializeField] private GameObject WinCross;
+    [SerializeField] private GameObject WinCircle;
 
     List<GameObject> graphicsHolder = new List<GameObject>();
     Vector2[] plotLocation = new Vector2[]
-        {
-            new Vector2(-2,2),
-            new Vector2(0,2),
-            new Vector2(2,2),
-            new Vector2(-2,0),
-            new Vector2(0,0),
-            new Vector2(2,0),
-            new Vector2(-2,-2),
-            new Vector2(0,-2),
-            new Vector2(2,-2),
-        };
+    {
+        new Vector2(-2,2),
+        new Vector2(0,2),
+        new Vector2(2,2),
+        new Vector2(-2,0),
+        new Vector2(0,0),
+        new Vector2(2,0),
+        new Vector2(-2,-2),
+        new Vector2(0,-2),
+        new Vector2(2,-2),
+    };
+    
+    Vector3[] winPosition = new Vector3[]
+    {
+        new Vector3(0,2,90),
+        new Vector3(0,0,90),
+        new Vector3(0,-2,90),
+        new Vector3(-2,0,0),
+        new Vector3(0,0,0),
+        new Vector3(2,0,0),
+        new Vector3(0,0,45),
+        new Vector3(0,0,-45)
+    };
 
     TextMeshProUGUI nameAI;
     TextMeshProUGUI scoreAI;
     TextMeshProUGUI namePlayer;
     TextMeshProUGUI scorePlayer;
 
+    void Update()
+    {
+       insInputAI.SettingsAI(AI);
+    }
+
     void Start()
     {
         insInputManager = FindObjectOfType<InputManager>();
+        insInputAI = FindObjectOfType<InputAI>();
 
         nameAI = GameObject.Find("AI").GetComponent<TMPro.TextMeshProUGUI>();
         scoreAI = GameObject.Find("CircleScores").GetComponent<TMPro.TextMeshProUGUI>();
@@ -76,6 +100,64 @@ public class GameManager : MonoBehaviour
                 Instantiate(Circle, plotLocation[insSelectedLocation], Quaternion.identity));
             }
         }      
+    }
+
+    public void WinHandler(bool isPlayerCross, int drawLocation, int playerID)
+    {
+        if(isPlayerCross && playerID == 1)
+        {
+            graphicsHolder.Add(
+            Instantiate(WinCircle, 
+            new Vector3(winPosition[drawLocation].x, winPosition[drawLocation].y,0), 
+            Quaternion.Euler(0,0,winPosition[drawLocation].z)));
+            
+            ScoreSystem(1);
+        }
+        else if(!isPlayerCross && playerID == 1)
+        {
+            graphicsHolder.Add(
+            Instantiate(WinCross, 
+            new Vector3(winPosition[drawLocation].x, winPosition[drawLocation].y,0), 
+            Quaternion.Euler(0,0,winPosition[drawLocation].z)));
+
+            ScoreSystem(2);
+        }
+        else if(isPlayerCross && playerID == 2)
+        {
+            graphicsHolder.Add(
+            Instantiate(WinCross, 
+            new Vector3(winPosition[drawLocation].x, winPosition[drawLocation].y,0), 
+            Quaternion.Euler(0,0,winPosition[drawLocation].z)));
+
+            ScoreSystem(2);
+        }
+        else if(!isPlayerCross && playerID == 2)
+        {
+            graphicsHolder.Add(
+            Instantiate(WinCircle, 
+            new Vector3(winPosition[drawLocation].x, winPosition[drawLocation].y,0), 
+            Quaternion.Euler(0,0,winPosition[drawLocation].z)));
+
+            ScoreSystem(1);
+        }
+    }
+
+    void ScoreSystem(int playerID)
+    {
+        if(playerID == 1)
+        {
+            int currentAIScore = System.Convert.ToInt32(scoreAI.text);
+            currentAIScore++;
+
+            scoreAI.text = System.Convert.ToString(currentAIScore);
+        }
+        else if(playerID == 2)
+        {
+            int currentPlayerScore = System.Convert.ToInt32(scorePlayer.text);
+            currentPlayerScore++;
+
+            scorePlayer.text = System.Convert.ToString(currentPlayerScore);
+        }
     }
 
     public void GameReset()
